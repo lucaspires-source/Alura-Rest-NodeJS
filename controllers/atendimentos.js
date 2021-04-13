040e602d73d390314c0160976453538a9803f38d
lucaspires-source <pires.lucas94@gmail.com>
2021-04-13 10:26:18 -0300

    02.03 Rota para croar atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,9 @@
 
 
 module.exports = app =>{
-    app.get('/atendimentos',(req,res) => res.send('Você está na rota de atendimentos e esta realizando um get'))
+    app.get('/atendimentos',(req,res) => res.send('Você está na rota de atendimentos e esta realizando um GET'))
+
+    app.post('/atendimentos',(req,res) =>{
+        res.send('Você está na rota de atendimentos e esta realizando um POST')
+    })  
 }
