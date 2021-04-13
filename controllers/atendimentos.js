e746d882f40f433a2bab03d3659d439bbd5f7028
lucaspires-source <pires.lucas94@gmail.com>
2021-04-13 15:20:09 -0300

    03.04 persistindo dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,13 @@
+const Atendimento = require('../models/atendimento')
+
 module.exports = (app) => {
   app.get("/atendimentos", (req, res) =>
     res.send("Você está na rota de atendimentos e esta realizando um GET")
   );
 
   app.post("/atendimentos", (req, res) => {
-     console.log(req.body) 
-    res.send("Você está na rota de atendimentos e esta realizando um POST");
+     const atendimento = req.body
+     Atendimento.adiciona(atendimento)
+      res.send("Você está na rota de atendimentos e esta realizando um POST");
   });
 };
