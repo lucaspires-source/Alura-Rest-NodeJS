270336ac46379b69e3ab7704df32cb4103744e2d
lucaspires-source <pires.lucas94@gmail.com>
2021-04-14 09:19:14 -0300

    04.03 Status HTTP

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -8,6 +8,6 @@ module.exports = (app) => {
   app.post("/atendimentos", (req, res) => {
      const atendimento = req.body
      Atendimento.adiciona(atendimento)
-      res.send("Você está na rota de atendimentos e esta realizando um POST");
+    
   });
 };
