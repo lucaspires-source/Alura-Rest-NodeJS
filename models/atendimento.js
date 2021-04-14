49bf1510afadbf71561bbcd2eb57136cae730628
lucaspires-source <pires.lucas94@gmail.com>
2021-04-14 09:58:29 -0300

    04.04 aplicando validações de entrada

--- models/atendimento.js
+++ models/atendimento.js
@@ -1,22 +1,47 @@
 const moment = require('moment')
 const conexao  = require('../infraestrutura/conexao');
 class Atendimento{
-    adiciona(atendimento){
+    adiciona(atendimento,res){
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado ={
-            ...atendimento,dataCriacao,data
-        }
-        const sql = 'INSERT INTO Atendimentos SET ?'
+        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteValido = atendimento.cliente.length > 1
 
-        conexao.query(sql,atendimentoDatado,(erro,resultados) =>{
-            if (erro){
-                res.status(400).json(erro)
-            }else{
-                res.status(201).json(resultados)
+        const validacao =[
+            {
+                nome:'data',
+                valido:dataValida,
+                mensagem:'Data deve ser maior ou igual a data atual'
+            },
+
+            {
+                nome:'cliente',
+                valido:clienteValido,
+                mensagem:'Cliente deve ter pelo menos 2 caracteres'
             }
-            
-        })
+        ]
+        
+        const erros = validacao.filter(campo => !campo.valido)
+
+        const existemErros = erros.length
+
+        if(existemErros){
+            res.status(400).json(erros)
+        }else{
+            const atendimentoDatado ={
+                ...atendimento,dataCriacao,data
+            }
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql,atendimentoDatado,(erro,resultados) =>{
+                if (erro){
+                    res.status(400).json(erro)
+                }else{
+                    res.status(201).json(resultados)
+                }
+                
+            })
+        }
     }
 }
 
