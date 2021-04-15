868d7fc631ded39cc27b02e83b05a737b6d8c6d4
lucaspires-source <pires.lucas94@gmail.com>
2021-04-15 09:23:36 -0300

    aula 02.04 Lidando com erros no tipo de imagens

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,17 +1,17 @@
-const fs = require('fs')
-const path = require('path')
-module.exports =(caminho, nomeDoArquivo,callbackImagemCriada)=>{
-    const tiposValidos = ['jpg','jpeg','png']
-    const tipo = path.extname(caminho)
-    const tipoValido =  tiposValidos.indexOf(tipo.substring(1))
-    if (tipoValido === -1){
-        console.log('Erro Tipo inválido')
-    }else{
-        const novoCaminho = `./assets/images/${nomeDoArquivo}${tipo}`
+const fs = require("fs");
+const path = require("path");
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+  const tiposValidos = ["jpg", "jpeg", "png"];
+  const tipo = path.extname(caminho);
+  const tipoValido = tiposValidos.indexOf(tipo.substring(1)) !== -1;
+  if (tipoValido) {
+    const novoCaminho = `./assets/images/${nomeDoArquivo}${tipo}`;
     fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))  
-    }
-    
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on("finish", () => callbackImagemCriada(false, novoCaminho));
+  } else {
 
-}
+      const erro = "Tipo inválido";
+      callbackImagemCriada(erro);
+  }
+};
--- models/pets.js
+++ models/pets.js
@@ -1,29 +1,30 @@
-const conexao = require('../infraestrutura/conexao')
-const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
+const conexao = require("../infraestrutura/conexao");
+const uploadDeArquivo = require("../arquivos/uploadDeArquivos");
 
+class Pet {
+  adiciona(pet, res) {
+    const query = "INSERT INTO Pets SET ?";
 
+    uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+      if (erro) {
+          res.status(400).json({erro})
+      } else {
+        const novoPet = {
+          nome: pet.nome,
+          imagem: novoCaminho,
+        };
 
-class Pet{
-    adiciona(pet,res){
-        const query = 'INSERT INTO Pets SET ?'
-
-        uploadDeArquivo(pet.imagem, pet.nome, (novoCaminho) =>{
-            const novoPet = {
-                nome:pet.nome,
-                imagem:novoCaminho
-            }
-            
-            conexao.query(query, novoPet,(erro) =>{
-                if(erro){
-                    console.log(erro)
-                    res.status(400).json(erro)
-                }else{
-                    res.status(200).json(novoPet)
-                }
-            })
-        })
-
-    }
+        conexao.query(query, novoPet, (erro) => {
+          if (erro) {
+            console.log(erro);
+            res.status(400).json(erro);
+          } else {
+            res.status(200).json(novoPet);
+          }
+        });
+      }
+    });
+  }
 }
 
-module.exports = new Pet()
+module.exports = new Pet();
